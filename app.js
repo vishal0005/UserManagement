ec345e0c49523a58f4f7b6de3f8883f2ffc571af
vishalneon <vishal.bhimporwala@neonscreens.com>
2024-01-22 22:23:01 +0530

    dot env added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/user-routes');
@@ -5,11 +6,12 @@ const app = express();
 
 app.use(express.json());
 app.use("/users",router);
+const PORT = process.env.PORT
 
 mongoose
 .connect(
     "mongodb+srv://vishalbhimporwala:vishalbhimporwala@vishal.uhwh9wg.mongodb.net/?retryWrites=true&w=majority"
     )
-    .then(()=>app.listen(4546,()=>
-    console.log("connectd and listening port 4546")))
+    .then(()=>app.listen(PORT,()=>
+    console.log("connectd and listening port ",PORT)))
     .catch((err)=>console.log("connection error ",err));
